de7333858defc40fcfa55649cccf17c4aaabf5cb
emagodi <magodiedwin@gmail.com>
2022-12-04 12:03:59 +0200

    Catch exceptions

--- src/main/java/solutions/fluidity/test/resource/FixturesResource.java
+++ src/main/java/solutions/fluidity/test/resource/FixturesResource.java
@@ -1,6 +1,7 @@
 package solutions.fluidity.test.resource;
 
 import org.springframework.stereotype.Component;
+import solutions.fluidity.test.exception.CustomRunTimeException;
 import solutions.fluidity.test.service.FixturesService;
 
 import javax.ws.rs.*;
@@ -22,7 +23,14 @@ public class FixturesResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response getFixtures() throws IOException {
-        String result = fixturesService.getFixtures();
-        return Response.ok(result).build();
+        try {
+            String result = fixturesService.getFixtures();
+            return Response.ok(result).build();
+        }catch (IOException ioe){
+            return Response.serverError().entity(ioe.getMessage()).build();
+        }catch (CustomRunTimeException crte){
+            return Response.serverError().entity(crte.getMessage()).build();
+        }
     }
+
 }
