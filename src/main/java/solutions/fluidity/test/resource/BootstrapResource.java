baf7b0fc487983eda233e39d5354b99521b8f9ca
emagodi <magodiedwin@gmail.com>
2022-12-04 12:01:39 +0200

    Add logger and catch errors

--- src/main/java/solutions/fluidity/test/resource/BootstrapResource.java
+++ src/main/java/solutions/fluidity/test/resource/BootstrapResource.java
@@ -1,6 +1,7 @@
 package solutions.fluidity.test.resource;
 
 import org.springframework.stereotype.Component;
+import solutions.fluidity.test.exception.CustomRunTimeException;
 import solutions.fluidity.test.service.BootstrapService;
 
 import javax.ws.rs.Consumes;
@@ -10,13 +11,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Component
 @Path("bootstrap")
 public class BootstrapResource {
 
     private BootstrapService bootstrapService;
-
+    final Logger log = Logger.getLogger(FixturesResource.class.getName());
     public BootstrapResource(BootstrapService bootstrapService) {
         this.bootstrapService = bootstrapService;
     }
@@ -25,7 +28,14 @@ public class BootstrapResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBootstrapStatic() throws IOException {
-        String result = bootstrapService.getBootstrapData();
-        return Response.ok(result).build();
+        try {
+            String result = bootstrapService.getBootstrapData();
+            return Response.ok(result).build();
+        }catch (IOException ioe){
+            log.log(Level.SEVERE,ioe.getMessage(),ioe.getCause());
+        }catch (CustomRunTimeException crte){
+            log.log(Level.SEVERE,crte.getMessage(),crte.getCause());
+        }
+        return Response.serverError().build();
     }
 }
