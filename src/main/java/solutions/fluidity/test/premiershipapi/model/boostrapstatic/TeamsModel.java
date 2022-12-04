c5c0e326b95d24f2c0ac59bb925f4e022ab9745f
emagodi <magodiedwin@gmail.com>
2022-12-04 11:28:24 +0200

    Add lombok and remove getters and setters

--- src/main/java/solutions/fluidity/test/premiershipapi/model/boostrapstatic/TeamsModel.java
+++ src/main/java/solutions/fluidity/test/premiershipapi/model/boostrapstatic/TeamsModel.java
@@ -1,22 +1,9 @@
 package solutions.fluidity.test.premiershipapi.model.boostrapstatic;
 
+import lombok.Data;
+
+@Data
 public class TeamsModel {
     private TeamModel homeTeam;
     private TeamModel awayTeam;
-
-    public TeamModel getHomeTeam() {
-        return homeTeam;
-    }
-
-    public void setHomeTeam(TeamModel homeTeam) {
-        this.homeTeam = homeTeam;
-    }
-
-    public TeamModel getAwayTeam() {
-        return awayTeam;
-    }
-
-    public void setAwayTeam(TeamModel awayTeam) {
-        this.awayTeam = awayTeam;
-    }
 }
